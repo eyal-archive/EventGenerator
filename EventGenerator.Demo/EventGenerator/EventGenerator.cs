e8377c885ba920811a5f69725ee657a87d488ce6
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-12-10 15:34:16 +0200

    first commit

---
+++ EventGenerator.Demo/EventGenerator/EventGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace EventGenerator;
+
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+
+[Generator]
+file sealed class EventGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+    }
+}
